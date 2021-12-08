725bae5e0064a55cf6cf64124496c866b614a5de
Zachary K Watkins <3458875+ZachWatkins@users.noreply.github.com>
2021-12-08 07:15:33 -0600

    Add default theme content using the Genesis Starter Pack feature

---
+++ config/onboarding.php
@@ -0,0 +1,129 @@
+<?php
+/**
+ * Genesis Sample.
+ *
+ * Onboarding config to present a choice of starter packs.
+ *
+ * @package Genesis Sample
+ * @author  StudioPress
+ * @license GPL-2.0-or-later
+ * @link    https://www.studiopress.com/
+ */
+return [
+	'starter_packs' => [
+		'pack-1' => [
+			'title'       => __( 'Required Content', 'cla-wsorder' ),
+			'description' => __( 'Required plugins, recommended plugins, and required content for the Workstation Ordering Application.', 'cla-wsorder' ),
+			'thumbnail'   => get_stylesheet_directory_uri() . '/config/import/images/thumbnails/pack-1.jpg',
+			'demo_url'    => 'https://github.com/zachwatkins/cla-wsorder/',
+			'config'      => [
+				'dependencies'     => [
+					[
+						'name'       => __( 'Advanced Custom Fields', 'cla-wsorder' ),
+						'slug'       => 'advanced-custom-fields/acf.php',
+						'public_url' => 'https://wordpress.org/plugins/advanced-custom-fields/',
+					],
+					[
+						'name'       => __( 'Post SMTP', 'cla-wsorder' ),
+						'slug'       => 'post-smtp/postman-smtp.php',
+						'public_url' => 'https://wordpress.org/plugins/post-smtp/',
+					],
+					[
+						'name'       => __( 'User Switching', 'cla-wsorder' ),
+						'slug'       => 'user-switching/user-switching.php',
+						'public_url' => 'https://wordpress.org/plugins/user-switching/',
+					],
+					[
+						'name'       => __( 'Simple History', 'cla-wsorder' ),
+						'slug'       => 'simple-history/index.php',
+						'public_url' => 'https://wordpress.org/plugins/simple-history/',
+					],
+					[
+						'name'       => __( 'Yoast Duplicate Post', 'cla-wsorder' ),
+						'slug'       => 'duplicate-post/duplicate-post.php',
+						'public_url' => 'https://wordpress.org/plugins/duplicate-post/',
+					],
+					[
+						'name'       => __( 'OneLogin SAML SSO', 'cla-wsorder' ),
+						'slug'       => 'onelogin-saml-sso/onelogin_saml.php',
+						'public_url' => 'https://wordpress.org/plugins/onelogin-saml-sso/',
+					],
+				],
+				'content'          => [
+					'homepage'    => [
+						'post_title'     => 'Home',
+						'post_content'   => require dirname( __FILE__ ) . '/import/content/home.php',
+						'post_type'      => 'page',
+						'post_status'    => 'publish',
+						'page_template'  => 'page-templates/page-home.php',
+						'comment_status' => 'closed',
+						'ping_status'    => 'closed',
+					],
+					'order-form'    => [
+						'post_title'     => 'Order Form',
+						'post_content'   => require dirname( __FILE__ ) . '/import/content/order-form.php',
+						'post_type'      => 'page',
+						'post_status'    => 'publish',
+						'comment_status' => 'closed',
+						'ping_status'    => 'closed',
+					],
+					'catalog'    => [
+						'post_title'     => 'Catalog',
+						'post_content'   => require dirname( __FILE__ ) . '/import/content/catalog.php',
+						'post_type'      => 'page',
+						'post_status'    => 'publish',
+						'comment_status' => 'closed',
+						'ping_status'    => 'closed',
+					],
+					'orders'    => [
+						'post_title'     => 'Orders',
+						'post_content'   => require dirname( __FILE__ ) . '/import/content/orders.php',
+						'post_type'      => 'page',
+						'post_status'    => 'publish',
+						'comment_status' => 'closed',
+						'ping_status'    => 'closed',
+					],
+					'my-account'    => [
+						'post_title'     => 'My Account',
+						'post_content'   => require dirname( __FILE__ ) . '/import/content/my-account.php',
+						'post_type'      => 'page',
+						'post_status'    => 'publish',
+						'comment_status' => 'closed',
+						'ping_status'    => 'closed',
+					],
+					'my-orders'    => [
+						'post_title'     => 'My Orders',
+						'post_content'   => require dirname( __FILE__ ) . '/import/content/my-orders.php',
+						'post_type'      => 'page',
+						'post_status'    => 'publish',
+						'comment_status' => 'closed',
+						'ping_status'    => 'closed',
+					],
+				],
+				'navigation_menus' => [
+					'primary' => [
+						'homepage'   => [
+							'title' => 'Home',
+						],
+						'order-form' => [
+							'title' => 'Order Form',
+						],
+						'catalog' => [
+							'title' => 'Catalog',
+						],
+						'orders' => [
+							'title' => 'Orders',
+						],
+						'my-account' => [
+							'title' => 'My Account',
+						],
+						'my-orders' => [
+							'title' => 'My Orders',
+						],
+					],
+				],
+				'widgets'          => [],
+			],
+		],
+	],
+];
