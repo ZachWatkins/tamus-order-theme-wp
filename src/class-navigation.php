627b13b546700ef4aa44b23e89fa476cac26c51e
Zachary K Watkins <3458875+ZachWatkins@users.noreply.github.com>
2021-12-09 02:24:41 -0600

    Move the Home Page Primary Navigation Menu Item to the left as the first link in the menu

--- src/class-navigation.php
+++ src/class-navigation.php
@@ -35,7 +35,8 @@ class Navigation {
 		if ( 'primary' === $args['theme_location'] ) {
 			$current_program_id = get_site_option( 'options_current_program' );
 			$orders_url         = admin_url( "edit.php?post_type=wsorder&program=$current_program_id" );
-			$output             = "<li class=\"brand\">Administrative Functions</li><li><a href=\"/orders/\">Orders</a></li>";
+			$menu_title         = "<li class=\"brand\">Administrative Functions</li>";
+			$output             = "<li><a href=\"/orders/\">Orders</a></li>";
 			if ( current_user_can( 'wso_admin' ) || current_user_can( 'wso_logistics' ) || current_user_can( 'wso_logistics_admin' ) ) {
 				$bundles_url     = admin_url( 'edit.php?post_type=bundle' );
 				$categories_url  = admin_url( 'edit-tags.php?taxonomy=product-category' );
@@ -46,7 +47,7 @@ class Navigation {
 				$settings_url    = admin_url( 'admin.php?page=wsorder-settings' );
 				$output          .= "<li><a href=\"$bundles_url\">Bundles</a></li><li><a href=\"$categories_url\">Categories</a></li><li><a href=\"$products_url\">Products</a></li><li><a href=\"$departments_url\">Departments</a></li><li><a href=\"$users_url\">Users</a></li><li><a href=\"$programs_url\">Programs</a></li><li><a href=\"$settings_url\">Settings</a></li>";
 			}
-			$menu = $output . $menu;
+			$menu = $menu_title . $menu . $output;
 		}
 
 		return $menu;
@@ -66,7 +67,8 @@ class Navigation {
 		if ( 'primary' === $args->theme_location ) {
 			$current_program_id = get_site_option( 'options_current_program' );
 			$orders_url         = admin_url( "edit.php?post_type=wsorder&program=$current_program_id" );
-			$output             = "<li class=\"brand\">Administrative Functions</li><li><a href=\"/orders/\">Orders</a></li>";
+			$menu_title         = "<li class=\"brand\">Administrative Functions</li>";
+			$output             = "<li><a href=\"/orders/\">Orders</a></li>";
 			if ( current_user_can( 'wso_admin' ) || current_user_can( 'wso_logistics' ) || current_user_can( 'wso_logistics_admin' ) ) {
 				$bundles_url     = admin_url( 'edit.php?post_type=bundle' );
 				$categories_url  = admin_url( 'edit-tags.php?taxonomy=product-category' );
@@ -77,7 +79,7 @@ class Navigation {
 				$settings_url    = admin_url( 'admin.php?page=wsorder-settings' );
 				$output          .= "<li><a href=\"$bundles_url\">Bundles</a></li><li><a href=\"$categories_url\">Categories</a></li><li><a href=\"$products_url\">Products</a></li><li><a href=\"$departments_url\">Departments</a></li><li><a href=\"$users_url\">Users</a></li><li><a href=\"$programs_url\">Programs</a></li><li><a href=\"$settings_url\">Settings</a></li>";
 			}
-			$menu = $output . $menu;
+			$menu = $menu_title . $menu . $output;
 		}
 
 		return $menu;
