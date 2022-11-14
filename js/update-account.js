f38b1b17fb19752287758ae9190326b1a32ee17f
Zachary K. Watkins <3458875+ZachWatkins@users.noreply.github.com>
2022-11-13 22:05:27 -0600

    Apply changes using WordPress coding standards to JS file

--- js/update-account.js
+++ js/update-account.js
@@ -8,79 +8,82 @@
  * @subpackage tamus-order-plugin-wp/js
  * @license    https://www.gnu.org/licenses/old-licenses/gpl-2.0.html GNU General Public License v2.0 or later
  */
- (function($){
+
+(function($){
 
 	var admin_ajax = WSOAjax;
-	var $form      = jQuery('#tamus_update_account_form');
-  var $response  = $form.find('.ajax-response');
-  var values     = {
-    first_name: $form.find('#tamus_first_name').val(),
-    last_name: $form.find('#tamus_last_name').val(),
-    email: $form.find('#tamus_email').val(),
-    department: $form.find('#tamus_department').val(),
-  };
+	var $form      = jQuery( '#tamus_update_account_form' );
+	var $response  = $form.find( '.ajax-response' );
+	var values     = {
+		first_name: $form.find( '#tamus_first_name' ).val(),
+		last_name: $form.find( '#tamus_last_name' ).val(),
+		email: $form.find( '#tamus_email' ).val(),
+		department: $form.find( '#tamus_department' ).val(),
+	};
 
 	var ajaxUpdateAccount = function(e){
-    e.preventDefault();
-    var form_data = new FormData();
-    var first_name = $form.find('#tamus_first_name').val();
-    var last_name  = $form.find('#tamus_last_name').val();
-    var email      = $form.find('#tamus_email').val();
-    var department = $form.find('#tamus_department').val();
-    // Determine if we need to make an AJAX call to update the user account details.
-    if (
-      first_name === values.first_name
-      && last_name === values.last_name
-      && email === values.email
-      && department === values.department
-      ) {
-      $response.html('<span class="notice-red">No changes were made.</span>');
-      return;
-    } else {
-      values.first_name = first_name;
-      values.last_name  = last_name;
-      values.email      = email;
-      values.department = department;
-      $response.html('');
-    }
-    form_data.append('first_name', first_name);
-    form_data.append('last_name', last_name);
-    form_data.append('email', email);
-    form_data.append('department', department);
-    form_data.append('action', 'update_acount');
-    form_data.append('_ajax_nonce', admin_ajax.nonce);
-    jQuery.ajax({
-      type: "POST",
-      url: admin_ajax.ajaxurl,
-			contentType: false,
-			processData: false,
-      data: form_data,
-      success: function(data) {
-        var response_output = '';
-      	if ( data.indexOf('{') === 0 ) {
-					// Only JSON returned.
-      		var response = JSON.parse(data);
-          var errors = response.errors;
-          if ( 'success' === response.status ) {
-            response_output += '<span class="notice-green">You have updated your account.</span>';
-            if ( errors.length > 0 ) {
-              // There was a success but also an error.
-              response_output += '<span class="notice-red">' + errors.join(' ') + '</span>';
-            }
-          } else {
-            response_output += '<span class="notice-red">' + errors.join(' ') + '</span>';
-          }
-      	} else {
-      		response_output += '<span class="notice-red">There was an error updating your account: ' + data + '</span>';
-      	}
-        $response.html(response_output);
-      },
-      error: function( jqXHR, textStatus, errorThrown ) {
-      	$response.html('<span class="notice-red">The application encountered an error while submitting your request (' + errorThrown + ').<br>' + jqXHR.responseText + '</span>');
-      }
-    });
-  };
+		e.preventDefault();
+		var form_data  = new FormData();
+		var first_name = $form.find( '#tamus_first_name' ).val();
+		var last_name  = $form.find( '#tamus_last_name' ).val();
+		var email      = $form.find( '#tamus_email' ).val();
+		var department = $form.find( '#tamus_department' ).val();
+		// Determine if we need to make an AJAX call to update the user account details.
+		if (
+		first_name === values.first_name
+		&& last_name === values.last_name
+		&& email === values.email
+		&& department === values.department
+		) {
+			$response.html( '<span class="notice-red">No changes were made.</span>' );
+			return;
+		} else {
+			values.first_name = first_name;
+			values.last_name  = last_name;
+			values.email      = email;
+			values.department = department;
+			$response.html( '' );
+		}
+		form_data.append( 'first_name', first_name );
+		form_data.append( 'last_name', last_name );
+		form_data.append( 'email', email );
+		form_data.append( 'department', department );
+		form_data.append( 'action', 'update_acount' );
+		form_data.append( '_ajax_nonce', admin_ajax.nonce );
+		jQuery.ajax(
+			{
+				type: "POST",
+				url: admin_ajax.ajaxurl,
+				contentType: false,
+				processData: false,
+				data: form_data,
+				success: function(data) {
+					var response_output = '';
+					if ( data.indexOf( '{' ) === 0 ) {
+						// Only JSON returned.
+						var response = JSON.parse( data );
+						var errors   = response.errors;
+						if ( 'success' === response.status ) {
+							response_output += '<span class="notice-green">You have updated your account.</span>';
+							if ( errors.length > 0 ) {
+								// There was a success but also an error.
+								response_output += '<span class="notice-red">' + errors.join( ' ' ) + '</span>';
+							}
+						} else {
+							response_output += '<span class="notice-red">' + errors.join( ' ' ) + '</span>';
+						}
+					} else {
+						response_output += '<span class="notice-red">There was an error updating your account: ' + data + '</span>';
+					}
+					$response.html( response_output );
+				},
+				error: function( jqXHR, textStatus, errorThrown ) {
+					$response.html( '<span class="notice-red">The application encountered an error while submitting your request (' + errorThrown + ').<br>' + jqXHR.responseText + '</span>' );
+				}
+			}
+		);
+	};
 
-	$form.find('#tamus_update_account').on('click', ajaxUpdateAccount);
+	$form.find( '#tamus_update_account' ).on( 'click', ajaxUpdateAccount );
 
-})(jQuery);
+})( jQuery );
